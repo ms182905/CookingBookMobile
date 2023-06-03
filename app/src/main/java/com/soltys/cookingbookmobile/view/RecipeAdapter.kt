3fc973b8f2484cc35740b9e6186f45d9ac3dcfcf
Maciej Soltys <maciej.soltys@student.wat.edu.pl>
2023-06-04 00:46:56 +0200

    Added displaying images in recipe list

--- app/src/main/java/com/soltys/cookingbookmobile/view/RecipeAdapter.kt
+++ app/src/main/java/com/soltys/cookingbookmobile/view/RecipeAdapter.kt
@@ -1,9 +1,6 @@
 package com.soltys.cookingbookmobile.view
 
 import android.app.Activity
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.soltys.cookingbookmobile.R
 import com.soltys.cookingbookmobile.model.Recipe
-import java.io.BufferedInputStream
-import java.io.IOException
-import java.io.InputStream
-import java.net.HttpURLConnection
-import java.net.URL
-import java.util.concurrent.Executors
-import java.util.logging.Handler
+import com.squareup.picasso.Picasso
 
 class RecipeAdapter(private val context : Activity, private val arrayList : ArrayList<Recipe>) : ArrayAdapter<Recipe>(
     context, R.layout.list_item, arrayList) {
@@ -30,39 +21,13 @@ class RecipeAdapter(private val context : Activity, private val arrayList : Arra
         val recipeName : TextView = view.findViewById(R.id.recipeName)
         val description : TextView = view.findViewById(R.id.recipeDescription)
 
-        val myExecutor = Executors.newSingleThreadExecutor()
-        var bitmap : Bitmap? = null
+        Picasso.with(context)
+            .load(arrayList[position].picture_url)
+            .into(imageView)
 
-        myExecutor.execute {
-            bitmap = (mLoad("https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/427506.jpg"))
-        }
-
-        imageView.setImageBitmap(bitmap)
         recipeName.text = arrayList[position].name
         description.text = arrayList[position].description
 
         return view
-    }
-
-    // Function to establish connection and load image
-    private fun mLoad(string: String): Bitmap? {
-        val url: URL = mStringToURL(string)!!
-        val connection: HttpURLConnection?
-        try {
-            connection = url.openConnection() as HttpURLConnection
-            connection.connect()
-            val inputStream: InputStream = connection.inputStream
-            val bufferedInputStream = BufferedInputStream(inputStream)
-            return BitmapFactory.decodeStream(bufferedInputStream)
-        } catch (e: IOException) {
-            e.printStackTrace()
-            //Toast.makeText(applicationContext, "Error", Toast.LENGTH_LONG).show()
-        }
-        return null
-    }
-
-    // Function to convert string to URL
-    private fun mStringToURL(string: String): URL? {
-            return URL(string)
     }
 }
